8226cb15982c5f9d1fcbc2d177cf270099304a09
Pranali Shinde <shindepranali466@gmail.com>
2025-06-11 20:02:48 +0530

    Add files via upload

---
+++ DuplicateElement.java
@@ -0,0 +1,37 @@
+/*
+	Check if an array contains duplicate elements
+	Check if the given array contains any duplicate values.
+*/
+
+import java.util.*;
+public class DuplicateElement
+{
+  public static void main(String x[])
+  {
+   Scanner xyz=new Scanner(System.in);
+   System.out.println("Enter Array Size");
+   int ar[]=new int[5];
+   for(int i=0;i<ar.length;i++)
+   {
+    a[i]=xyz.nextInt();
+}
+boolean b=true;
+for(int i=0;i<ar.length;i++)
+{
+  if(ar[i]==ar[j])
+  {
+   b=false;
+   break;
+   }
+ }
+}
+ if(b)
+ {
+
+  System.out.println("Not A Duplicate Element In Array");
+} else{
+   System.out.println("Duplicate element in array");
+   }
+ }
+}
+
